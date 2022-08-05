7552f55c5d7758db8ad7cbc5fdec6299d3f4d54a
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-08-06 01:48:12 +0900

    Chapter04-05

---
+++ src/main/java/com/dayone/Application.java
@@ -0,0 +1,20 @@
+package com.dayone;
+
+import com.dayone.model.Company;
+import com.dayone.scraper.Scraper;
+import com.dayone.scraper.YahooFinanceScraper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+//        Scraper scraper = new YahooFinanceScraper();
+////        var result = scraper.scrap(Company.builder().ticker("MMM").build());
+//        var result = scraper.scrapCompanyByTicker("MMM");
+//        System.out.println(result);
+    }
+}
